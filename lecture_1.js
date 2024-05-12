4228ba6d02800dd6cf4a1a33a527cc5428eafb61
Sanaullah23 <sanaullahb.019@gmail.com>
2024-05-12 16:27:33 +0500

    lecture -1 of javascript

---
+++ lecture_1.js
@@ -0,0 +1,68 @@
+/*
+as we know javascript is verstile language for multipurpose
+you can use Js for mobile app development to web development and 
+also for desktop application development and many more 
+js is Queen of languages
+*/
+
+// display text using console.log()
+
+console.log("my name is javascript")
+
+//let's now display text in alert box don't need to inspect webpage look for console
+
+window.alert('hello i am javascript do you know me')
+
+/*let's create variables in javascript
+ to create variable in javascript we mostly use 
+ variable is container which stores the value dynamic or static
+ let
+ var
+ const 
+ they have different purpose for use go search for them?
+*/
+
+let Name = 'sanaullah';
+console.log(Name)
+var Age=25;
+console.log(Age)
+const subject ='javascript programming';
+console.log(subject)
+
+// let's discuss about types of variables
+
+//Number variables
+//String variables
+//Boolean Variables
+
+let isStudent =false //this boolean data type of variable 
+
+console.log(typeof isStudent)
+console.log(typeof Name)
+console.log(typeof Age)
+console.log(typeof subject)
+
+
+/// let's now learning about some basic math operations
+// +
+// -
+// *
+// \
+// %
+// ** 
+
+let num1=10;
+let num2 = 20;
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+
+// similar to basic math opearations there are Assignment operators
+
+// +=, -=, *=, /=, %= , **=
+
+// this means  num1+=num2
+
