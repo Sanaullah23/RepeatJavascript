3a6a5e3551fabe1efd94f880fa28ec40289f53a7
Sanaullah23 <sanaullahb.019@gmail.com>
2024-05-12 16:49:50 +0500

    get html element and set js variables values

---
+++ lecture_2.js
@@ -0,0 +1,14 @@
+/*
+let's now  get html elements and set javascript 
+variables data in them
+*/
+
+// you need to first get html element so use selector to select html tags
+
+let Name = "Sanaullah";
+let Age = 25;
+let Subject ="JavaScript Programming";
+
+document.getElementById('name').textContent=Name;
+document.getElementById('age').textContent=Age;
+document.getElementById('subject').textContent=Subject;
