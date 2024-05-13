cd37b1d917170e0cc64057fc950910befb041163
Sanaullah23 <sanaullahb.019@gmail.com>
2024-05-13 15:33:03 +0500

    Math object of javascript

---
+++ lecture_4.js
@@ -0,0 +1,18 @@
+/*
+ let's look at (Math) object  which provides many methods 
+ for mathematic operations
+*/
+
+let number = 20;
+let number1= 3;
+let number2 =3.437
+let extraNum= Math.floor(number2) // output 3
+
+
+console.log(Math.round(number2)) // output 3
+console.log(Math.ceil(number2)) // output 4
+console.log(Math.pow(number, number1)) // output 8000
+console.log(Math.sqrt(number2)) // 1.8 ...
+console.log(Math.round(Math.random()*10) )
+
+// there are many other methods explor them 
