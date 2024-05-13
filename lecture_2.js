fa65b99f6fed8cd91d3938a5f9dccaa7073a4ced
Sanaullah23 <sanaullahb.019@gmail.com>
2024-05-13 14:50:40 +0500

    add type conversion little bit

--- lecture_2.js
+++ lecture_2.js
@@ -12,3 +12,25 @@ let Subject ="JavaScript Programming";
 document.getElementById('name').textContent=Name;
 document.getElementById('age').textContent=Age;
 document.getElementById('subject').textContent=Subject;
+
+
+//type conversion of variablle 
+
+/* 
+ string into number
+ number into string
+ boolean into number etc
+*/
+
+let num =20;
+let num1 = 2;
+num =String(num)
+num+=10
+console.log(num)
+
+let text= 'sanaullah';
+text= Number(text)// find out what is problem why it can't convert string into number ?
+console.log(text)
+
+
+
