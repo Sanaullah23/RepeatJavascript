7a3beed4ba263162d601c48e0c209787dec8ac3c
Sanaullah23 <sanaullahb.019@gmail.com>
2024-05-15 11:05:39 +0500

    String  and its methods

---
+++ interview/reduceFunction.js
@@ -0,0 +1,6 @@
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+
+let signalValue=numbers.reduce((acc, current) => acc+current,0)
+console.log(signalValue);
+
+
---
+++ lecture_5.js
@@ -0,0 +1,40 @@
+/*
+let's start now learning string methods
+to manip the string and get modified string
+methods
+startWith()
+endWith()
+charAt()
+indexOf()
+slice()
+toUpperCase()
+toLowerCase()
+find()
+*/
+
+
+const myText= "I am Your Best Friend";
+//toUpperCase()
+let toUpperText= myText.toUpperCase();
+console.log(toUpperText)
+//toLowerCase()
+let toLowertext=myText.toLocaleLowerCase();
+console.log(toLowertext)
+
+//slice() from main string
+
+let sliceString= myText.slice(0, 5)
+console.log(sliceString);
+
+//charAt() to find  character at postion
+
+let charAtText= myText.charAt(0);
+console.log(charAtText)
+
+
+/*
+use remaining method of string each method is used for 
+specific purpose and the way of useage is similar as above 
+are used.
+
+*/
